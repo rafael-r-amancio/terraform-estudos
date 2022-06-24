09fc4ff1ec0cb379768605b8f90bb5d27d20233c
Rafael Amancio <rafael_r_amancio@outlook.com>
2022-06-24 19:28:15 -0300

    Criando servidor Django com Ansible e Terraform

--- main.tf
+++ main.tf
@@ -19,14 +19,14 @@ resource "aws_instance" "app_server" {
   ami           = "ami-0ddf424f81ddb0720"
   instance_type = "t2.micro"
   key_name = "iac-alura"
-  user_data = <<-EOF
-                    #!/bin/bash
-                    cd /home/ubuntu
-                    echo "<h1>Feito com Terraform</h1>" > index.html
-                    nohup busybox httpd -f -p 8080 &
-                EOF
+#   user_data = <<-EOF
+#                     #!/bin/bash
+#                     cd /home/ubuntu
+#                     echo "<h1>Feito com Terraform</h1>" > index.html
+#                     nohup busybox httpd -f -p 8080 &
+#                 EOF
 
   tags = {
-    Name = "Teste AWS"
+    Name = "Terraform Ansible Python"
   }
 }
